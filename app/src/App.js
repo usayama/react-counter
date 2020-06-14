adea6deeaccace9221e017db65d1315b73776ee0
y-nakane <y-nakane@sakura.ad.jp>
2020-06-14 23:34:22 +0900

    Reactでカウンターアプリ作成

---
+++ app/src/App.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import './App.css'
+
+// ページ（親）
+function App() {
+  return (
+    <div className="App">
+      <Factory />
+    </div>
+  )
+}
+
+// コンポーネント（子）
+class Factory extends React.Component {
+  // Vueでいう Template
+  render() {
+    return (
+      <div className="container">
+        <CounterList counters={this.state.counters} countUp={this.countUp} />
+        <div>TOTAL INVENTORY: {this.state.total}</div>
+      </div>
+    )
+  }
+  // Vueでいう Data
+  constructor() {
+    super()
+    this.state = {
+      counters: [
+        { id: 'A', count: 0, color: 'tomato' },
+        { id: 'B', count: 0, color: 'skyblue' },
+        { id: 'C', count: 0, color: 'limegreen' },
+        { id: 'D', count: 0, color: 'gray' },
+        { id: 'E', count: 0, color: 'pink' }
+      ],
+      total: 0
+    }
+    this.countUp = this.countUp.bind(this)
+  }
+  // Vueでいう Methods
+  countUp(counter) {
+    this.setState(prevState => {
+      const counters = prevState.counters.map(counter => {
+        return { id: counter.id, count: counter.count, color: counter.color }
+      })
+      const pos = counters
+        .map(counter => {
+          return counter.id
+        })
+        .indexOf(counter.id)
+      counters[pos].count++
+      return {
+        counters: counters,
+        total: prevState.total + 1
+      }
+    })
+  }
+}
+
+// コンポーネント（孫）
+function CounterList(props) {
+  const counters = props.counters.map(counter => {
+    return <Counter counter={counter} key={counter.id} countUp={props.countUp} />
+  })
+  return <ul>{counters}</ul>
+}
+
+// コンポーネント（ひ孫）
+function Counter(props) {
+  return (
+    <li
+      style={{ backgroundColor: props.counter.color }}
+      onClick={() => props.countUp(props.counter)}
+    >
+      {props.counter.id}:{props.counter.count}
+    </li>
+  )
+}
+
+export default App
